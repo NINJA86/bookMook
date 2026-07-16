2a141aa71aa0a6a1b85c8d6efe103746212d0d36
codingHisla <mohammadbabaye8989@gmail.com>
2026-07-16 20:24:32 +0330

    fix: fix book.controller.ts bug

--- backend/src/controller/book.controller.ts
+++ backend/src/controller/book.controller.ts
@@ -1,10 +1,7 @@
 import { Model } from 'mongoose';
-import bookModel from '../model/book.model';
 import { IBook } from '../lib/data';
 import { asyncHandler } from '../lib/funcs';
-import { NextFunction, Request, Response } from 'express';
-
-const book: Model<IBook> = bookModel;
+import { bookModel } from '../model';
 
 // -------------------- CONTROLLERS --------------------
 
@@ -13,10 +10,11 @@ export const getBookBySlug = asyncHandler(async (req, res) => {
 
   const { slug } = req.params;
 
-  const findBookBySlug = await book
+  const findBookBySlug = await bookModel
     .findOne({ slug })
     .populate('author')
     .populate('category');
+
   if (!findBookBySlug) {
     return res.status(404).json({
       message: 'Book not found',
@@ -28,7 +26,9 @@ export const getBookBySlug = asyncHandler(async (req, res) => {
 });
 
 export const getAllBooks = asyncHandler(async (req, res) => {
-  const books = await book.find({}).populate('author').populate('category');
-
+  const books = await bookModel
+    .find({})
+    .populate('author')
+    .populate('category');
   return res.status(200).json(books);
 });
